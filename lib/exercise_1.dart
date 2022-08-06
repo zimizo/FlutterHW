bae214f938a45b1d8a2ad4d589f79d79d2259615
zimizo <ziatdinovibrahim@gmail.com>
2022-08-06 10:02:37 +0500

    Практическая 6 с исправлениями

--- lib/exercise_1.dart
+++ lib/exercise_1.dart
@@ -1,37 +1,7 @@
 import 'package:flutter/material.dart';
 
-class ExerciseOne extends StatefulWidget {
+class ExerciseOne extends StatelessWidget {
   const ExerciseOne({Key? key}) : super(key: key);
-
-  @override
-  State<ExerciseOne> createState() => _ExerciseOneState();
-}
-
-class ListItem {
-  String title;
-  ListItem(this.title);
-}
-
-class _ExerciseOneState extends State<ExerciseOne>
-    with TickerProviderStateMixin {
-  final List<String> nav = ['Галерея', 'Мои фото'];
-  List<Widget> itemsList = List<Widget>.generate(
-      200,
-      (i) => ListTile(
-            title: Text('Item $i'),
-            onTap: () {},
-            shape: const Border(
-              bottom: BorderSide(
-                width: 1,
-                color: Color.fromARGB(115, 138, 135, 135),
-              ),
-            ),
-          ));
-
-  @override
-  void initState() {
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,8 +12,20 @@ class _ExerciseOneState extends State<ExerciseOne>
       body: Column(
         children: [
           Expanded(
-            child: ListView(
-              children: [for (final item in itemsList) item],
+            child: ListView.builder(
+              itemCount: 200,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text('Item $index'),
+                  onTap: () {},
+                  shape: const Border(
+                    bottom: BorderSide(
+                      width: 1,
+                      color: Color.fromARGB(115, 138, 135, 135),
+                    ),
+                  ),
+                );
+              },
             ),
           ),
         ],
--- lib/exercise_2.dart
+++ lib/exercise_2.dart
@@ -1,18 +1,7 @@
 import 'package:flutter/material.dart';
 
-class ExerciseTwo extends StatefulWidget {
+class ExerciseTwo extends StatelessWidget {
   const ExerciseTwo({Key? key}) : super(key: key);
-
-  @override
-  State<ExerciseTwo> createState() => _ExerciseTwoState();
-}
-
-class _ExerciseTwoState extends State<ExerciseTwo>
-    with TickerProviderStateMixin {
-  @override
-  void initState() {
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
