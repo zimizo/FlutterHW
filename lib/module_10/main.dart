dfb8c3cfb272425f3f4679cf8c3d965b75b93f2a
zimizo <ziatdinovibrahim@gmail.com>
2022-08-23 22:08:38 +0500

    Подготовил файлы для выполнения задания

---
+++ lib/module_10/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_home_work/module_10/views/login_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (BuildContext context) => const LoginView(),
+      },
+    );
+  }
+}
---
+++ lib/module_10/views/login_view.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_home_work/module_10/components/login_form.dart';
+import 'package:flutter_home_work/module_10/components/register_form.dart';
+
+enum FormType { login, register }
+
+class LoginView extends StatefulWidget {
+  const LoginView({Key? key}) : super(key: key);
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  FormType _formType = FormType.login;
+
+  _switchForm() {
+    setState(() {
+      _formType =
+          _formType == FormType.login ? FormType.register : FormType.login;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Expanded(
+                  child: Column(
+                    children: [
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          _formType == FormType.login ? 'Вход' : 'Регистрация',
+                          style: Theme.of(context).textTheme.headline4,
+                          textAlign: TextAlign.start,
+                        ),
+                      ),
+                      _formType == FormType.login
+                          ? const LoginForm()
+                          : const RegisterForm(),
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      _formType == FormType.login
+                          ? 'Уже есть аккаунт?'
+                          : 'Еще нет аккаунта? ',
+                    ),
+                    TextButton(
+                      onPressed: _switchForm,
+                      child: RichText(
+                        text: TextSpan(children: [
+                          TextSpan(
+                            text: _formType == FormType.login
+                                ? 'Войти'
+                                : 'Регистрация',
+                          )
+                        ], style: Theme.of(context).textTheme.bodyText1),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
