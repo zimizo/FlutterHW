88804c79aa2e37a423ac6975a31bc3dc951f07ec
zimizo <ziatdinovibrahim@gmail.com>
2022-08-29 19:51:48 +0500

    Файлы для 11 практической

---
+++ lib/module_11/data.dart
@@ -0,0 +1,52 @@
+List<Map<String, dynamic>> data = [
+  {
+    'id': 1,
+    'image': 'heic0406a.jpg',
+    'title': '',
+  },
+  {
+    'id': 2,
+    'image': 'heic0109a.jpg',
+    'title': '',
+  },
+  {
+    'id': 3,
+    'image': 'heic0506a.jpg',
+    'title': '',
+  },
+  {
+    'id': 4,
+    'image': 'heic0702a.jpg',
+    'title': '',
+  },
+  {
+    'id': 5,
+    'image': 'heic1509a.jpg',
+    'title': '',
+  },
+  {
+    'id': 6,
+    'image': 'heic0206b.jpg',
+    'title': '',
+  },
+  {
+    'id': 7,
+    'image': 'heic1608a.jpg',
+    'title': '',
+  },
+  {
+    'id': 8,
+    'image': 'heic2007a.jpg',
+    'title': '',
+  },
+  {
+    'id': 9,
+    'image': 'opo0328a.jpg',
+    'title': '',
+  },
+  {
+    'id': 10,
+    'image': 'potw2049a.jpg',
+    'title': '',
+  },
+];
